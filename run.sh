292d1f0b0aa81186126d0c37195f06c8c0255c86
damodaran <g.damodaran26@gmail.com>
2023-04-18 23:24:22 +1000

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
-xdev = $(buildkite-agent meta-data get xdev)
-echo "xdev =" $xdev
+echo "xdev =" $(buildkite-agent meta-data get xdev)
 
-xqa = $(buildkite-agent meta-data get xqa)
-echo "xqa =" $xqa
+echo "xqa =" $(buildkite-agent meta-data get xqa)
 
 echo "Start the docker build"
 docker build --tag mytest .
 
 echo "Start running the docker container"
-docker run --rm --env xdev=$xdev --env xqa=$xqa mytest
+docker run --rm --env xdev=$(buildkite-agent meta-data get xdev) --env xqa=$(buildkite-agent meta-data get xqa) mytest
