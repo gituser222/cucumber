0265dbaa5048955be7b3ca518a184f9e1c54ab0a
Damodaran G <damodaran_g@yahoo.com>
2024-06-22 21:04:38 +1000

    add sudo

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo $x
 echo "xqa =" $(buildkite-agent meta-data get xqa)
 
 echo "Start the docker build"
-docker build --tag mytest .
+sudo docker build --tag mytest .
 
 echo "Start running the docker container"
-docker run --rm --env xdev=$(buildkite-agent meta-data get xdev) --env xqa=$(buildkite-agent meta-data get xqa) mytest
+sudo docker run --rm --env xdev=$(buildkite-agent meta-data get xdev) --env xqa=$(buildkite-agent meta-data get xqa) mytest
