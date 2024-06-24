270d5037be09a45faca09565746b1ead3a8a7de3
Damodaran G <damodaran_g@yahoo.com>
2024-06-24 22:00:46 +1000

    Replace source with .

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 cucumber --tag @regression -f pretty -f json -o results.json
 report_builder -o test-report
 echo "upload report start"
-source upload.sh
+. upload.sh
 echo "upload report completed"
