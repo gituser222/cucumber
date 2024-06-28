7041e47a65383424e295d72375936f2e944bb3b8
Damodaran G <damodaran_g@yahoo.com>
2024-06-28 19:45:49 +1000

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,6 @@
 cucumber --tag @regression -f pretty -f json -o results.json
 report_builder -o test-report
 echo "upload report start"
-. upload.sh
+chmod +x ./upload.sh
+./upload.sh
 echo "upload report completed"
